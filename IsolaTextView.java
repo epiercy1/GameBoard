920e0a48f0d8f30b4e282edc42985b57a30377aa
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-09-15 11:25:25 -0400

    Add files via upload

---
+++ IsolaTextView.java
@@ -0,0 +1,23 @@
+
+public class IsolaTextView {
+	private IsolaBoard board;
+	public IsolaTextView(IsolaBoard board) {
+		this.board = board;
+	}
+	public void display() {
+		for(int row = 0; row < board.getHeight(); row++) {
+			for(int col = 0; col < board.getWidth(); col++) {
+				BoardSpace which = board.get(row, col);
+				if(which == BoardSpace.Available)
+					System.out.print("_ ");
+				if(which == BoardSpace.Player1)
+					System.out.print("1 ");
+				if(which == BoardSpace.Player2)
+					System.out.print("2 ");
+				if(which == BoardSpace.Missing)
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
